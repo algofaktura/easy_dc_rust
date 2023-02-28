b6cce300e7ecb07126fa32724ee32e91645588c3
Rommelo Yu <developer@discocube.org>
2023-02-28 17:56:50 +0100

    almost there!

--- src/main.rs
+++ src/main.rs
@@ -18,40 +18,28 @@ use crate::graphs::stratify::shrink_adjacency;
 use crate::graphs::make_weights::make_weights;
 use crate::graphs::graph32::GRAPH;
 use crate::graphs::translate::{graph_to_map, translate_verts_3d, make_vi_mapping};
-use crate::info::certify::SequenceID::HamCycle;
-use crate::info::certify::id_seq;
 use crate::operators::cut::cut;
 use crate::operators::spin::spin;
 use crate::operators::wind::wind;
 use crate::structs::vector2d::translate_from_nodes;
 use crate::structs::vector3d::Vector3D;
 
-
 fn main() {
-    let verts = &VERTS_32.iter().clone().map(|&(x, y, _)| (x, y)).collect::<Vec<_>>();
+    let _woven: Vec<VecDeque<u32>> = weave();
+}
+
+fn weave() -> Vec<VecDeque<u32>> {
     let v3verts: &Vec<Vector3D> = &translate_verts_3d(&VERTS_32);
     let adj: HashMap<u32, HashSet<u32>> = graph_to_map(&GRAPH);
     let (z_adj, z_length) = shrink_adjacency(&v3verts, &adj);
-    let weights: HashMap<u32, i32> = make_weights(&z_adj, &VERTS_32);
-
-    let path: Vec<u32> = spin(&z_adj, &weights);
-
-    let seq: [u32; 12] = path.iter().map(|&x| x as u32).collect::<Vec<u32>>().try_into().unwrap();
-    assert_eq!(HamCycle, id_seq(&seq, &adj));
-
-    let natural: Array2<i32> = translate_from_nodes(path, &verts);
-    let colored: Array2<i32> = color(&natural);
-
-    let spool: HashMap<u32, Array2<i32>> = HashMap::from([(3, natural), (1, colored)]);
+    let spool: HashMap<u32, Array2<i32>> = spool_yarn(z_adj);
     let vert_idx: HashMap<&Vector3D, u32> = make_vi_mapping(&v3verts);
     
     let mut bobbins: Vec<u32> = Vec::new();
     let mut loom: Vec<VecDeque<u32>> = Vec::new();
     
-
     #[allow(unused_assignments)]
     let mut warps = Vec::new();
-
 
     for (zlevel, order) in &z_length {
         let mut woven: HashSet<usize> = HashSet::new();
@@ -94,7 +82,6 @@ fn main() {
         if *zlevel != -1 {
             bobbins = wind(&mut loom, v3verts, &vert_idx);
         }
-
     }
 
     for w in &mut loom {
@@ -104,6 +91,15 @@ fn main() {
         w.extend(nodes.into_iter().rev());
     }
     loom.sort_by_key(|w| w.len());
-    // loom
+    loom
     
+}
+
+fn spool_yarn(z_adj: HashMap<u32, HashSet<u32>>) -> HashMap<u32, Array2<i32>>{
+    let verts = &VERTS_32.iter().clone().map(|&(x, y, _)| (x, y)).collect::<Vec<_>>();
+    let weights: HashMap<u32, i32> = make_weights(&z_adj, &VERTS_32);
+    let path: Vec<u32> = spin(&z_adj, &weights);
+    let natural: Array2<i32> = translate_from_nodes(path, &verts);
+    let colored: Array2<i32> = color(&natural);
+    HashMap::from([(3, natural), (1, colored)])
 }
