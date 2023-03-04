d9e50f61a7927e0383bf7f574880318fac3b87a8
Rommelo Yu <developer@discocube.org>
2023-03-04 02:54:54 +0100

    corrected wrong calculation of path from spin() which was unnoticed beforehand in the python version as set in python was more ordered. resulting rust version is more robust as it depends less on the ordering of the neighbors in the adjacency.

--- src/operators/spin.rs
+++ src/operators/spin.rs
@@ -33,47 +33,61 @@ pub fn get_edge_axis(m_vert: &[i32; 3], n_vert: &[i32; 3]) -> Axis {
     }
 }
 
-pub fn edge_axis(m_vert: &[i32; 3], n_vert: &[i32; 3]) -> usize {
-    (0..2).find(|&i| m_vert[i] != n_vert[i]).unwrap()
+pub fn get_axis(m_vert: &[i32; 3], n_vert: &[i32; 3]) -> usize {
+    (0..2).find(|&i| m_vert[i] != n_vert[i]).expect("VERTS ARE SIMILAR")
 }
 
 pub fn spin(
     adj_map: &HashMap<u32, HashSet<u32>>,
     weights: &HashMap<u32, i32>,
-    verts: &[[i32; 3]; 448],
+    verts: &[[i32; 3]],
 ) -> Vec<u32> {
     let path = &mut vec![*adj_map.keys().max().unwrap() as u32];
     let order = adj_map.len();
     let limit: usize = order - 5;
     for idx in 1..order {
         path.push(if idx < limit {
-            get_next(path.to_vec(), adj_map, weights)
+            get_next(&path, adj_map, weights)
         } else {
-            get_next_xyz(path.to_vec(), adj_map, weights, verts)
+            get_next_xyz(&path, adj_map, weights, verts)
         })
     }
     path.to_vec()
 }
 
 pub fn get_next(
-    path: Vec<u32>,
+    path: &[u32],
     adj_map: &HashMap<u32, HashSet<u32>>,
     weights: &HashMap<u32, i32>,
 ) -> u32 {
     adj_map
         .get(path.last().unwrap())
         .unwrap()
-        .difference(&path.iter().cloned().collect::<HashSet<u32>>())
-        .cloned()
+        .iter()
+        .filter(|n| !path.contains(*n))
+        .copied()
         .max_by_key(|&n| *weights.get(&n).unwrap())
         .unwrap()
 }
 
-pub fn get_next_xyz(
-    path: Vec<u32>,
+pub fn get_next1(
+    path: &[u32],
     adj_map: &HashMap<u32, HashSet<u32>>,
     weights: &HashMap<u32, i32>,
-    verts: &[[i32; 3]; 448],
+) -> u32 {
+    *adj_map
+        .get(path.last().expect("Path is empty"))
+        .expect("No adjacent vertices found")
+        .difference(&path.iter().copied().collect::<HashSet<u32>>())
+        .max_by_key(|&n| weights.get_key_value(&n).unwrap().1)
+        .expect("No unvisited adjacent vertices found")
+}
+
+pub fn get_next_xyz(
+    path: &[u32],
+    adj_map: &HashMap<u32, HashSet<u32>>,
+    weights: &HashMap<u32, i32>,
+    verts: &[[i32; 3]],
 ) -> u32 {
     let curr = path.last().unwrap();
     let curr_vert = &verts[*curr as usize];
@@ -83,9 +97,9 @@ pub fn get_next_xyz(
         .unwrap()
         .iter()
         .filter(|n| !path.contains(*n))
-        .map(|&n| (n, edge_axis(curr_vert, &verts[n as usize])))
-        .filter(|(_, next_axis)| *next_axis != edge_axis(prev_vert, curr_vert))
-        .max_by_key(|(n, _)| weights[n])
+        .map(|&n| (n, get_axis(curr_vert, &verts[n as usize])))
+        .filter(|(_, next_axis)| *next_axis != get_axis(prev_vert, curr_vert))
+        .max_by_key(|&(n, _)| weights[&n])
         .unwrap()
         .0
 }
