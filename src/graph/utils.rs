c4c435727cb4d3402667fc4b19dcc744d0c3a480
Rommelo Yu <developer@discocube.org>
2023-03-13 19:56:29 +0100

    cleanup

--- src/graph/utils.rs
+++ src/graph/utils.rs
@@ -1,31 +1,11 @@
-use ndarray::{arr2, Array2};
 use std::time::{Duration, Instant};
 
-use super::types::{Idx, Point, V3d, Vert, Yarn, Adjacency};
+use super::types::{Idx, Point, V3d, Vert, Adjacency};
 
 pub fn get_axis(m_vert: &V3d, n_vert: &V3d) -> Idx {
     (0..2)
         .find(|&i| m_vert[i] != n_vert[i])
         .expect("VERTS ARE SIMILAR")
-}
-
-pub fn reflect(a: &Yarn) -> Yarn {
-    a.clone().dot(&arr2(&[[-1, 0], [0, -1]]))
-}
-
-pub fn shift_2y(a: Yarn) -> Yarn {
-    a + arr2(&[[0, 2]])
-}
-
-pub fn shift_xyz(vert: Array2<Point>) -> Array2<Point> {
-    vert + arr2(&[
-        [2, 0, 0],
-        [-2, 0, 0],
-        [0, 2, 0],
-        [0, -2, 0],
-        [0, 0, 2],
-        [0, 0, -2],
-    ])
 }
 
 pub fn absumv((x, y, z): Vert) -> Point {
--- src/main.rs
+++ src/main.rs
@@ -13,33 +13,31 @@ use graph::utils;
 use graph::utils::get_order_from_n;
 use graph::weave;
 
-fn main() {
-    for n in 100..111 {
-        find_solution(make_graph(n, 1))
-    }
-}
+// pub fn solve_range() {
+//     for n in 100..111 {
+//         find_solution(make_graph(n), 1)
+//     }
+// }
 
-pub fn solve_from_args() {
+pub fn main() {
     // (n, order): (1, 8), (2, 32)...(100, 1,373,600), (200, 10,827,200), (300, )
     // cargo run --release 100 10
     let args: Vec<String> = env::args().collect();
-    find_solution(make_graph(
-            args.get(1)
-                .unwrap_or(&"100".to_string())
-                .parse()
-                .unwrap_or(100),
-            args.get(2)
-                .unwrap_or(&"1".to_string())
-                .parse()
-                .unwrap_or(1)
-        ))
+    let n: u32 = args.get(1)
+        .unwrap_or(&"100".to_string())
+        .parse()
+        .unwrap_or(100);
+    let repeats: u32 = args.get(2)
+        .unwrap_or(&"1".to_string())
+        .parse()
+        .unwrap_or(1);
+    let graph = make_graph(n);
+    find_solution(graph, repeats)
 }
 
 pub fn make_graph(
     n: u32,
-    repeats: u32,
 ) -> (
-    u32,
     u32,
     u32,
     Verts,
@@ -57,12 +55,11 @@ pub fn make_graph(
     let edge_adj: EdgeAdjacency =
         make::edges_adjacency_mapping(&adj, &verts);
     let (z_adj, z_order) = shrink::adjacency(&verts, &adj);
-    (n, order, repeats, verts, vi_map, adj, edge_adj, z_adj, z_order)
+    (n, order, verts, vi_map, adj, edge_adj, z_adj, z_order)
 }
 
 pub fn find_solution(
-    (n, order, repeats, verts, vi_map, adj, edge_adj, z_adj, z_order): (
-        u32,
+    (n, order, verts, vi_map, adj, edge_adj, z_adj, z_order): (
         u32,
         u32,
         Verts,
@@ -71,7 +68,7 @@ pub fn find_solution(
         EdgeAdjacency,
         Adjacency,
         ZOrder,
-    ),
+    ), repeats: u32
 ) {
     let mut solution: Solution = Solution::new();
     let start: Instant = Instant::now();
