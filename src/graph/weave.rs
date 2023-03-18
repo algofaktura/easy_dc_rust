2adc0b16fabb7db7f5e56cceeff5b2a938cd6848
Rommelo Yu <developer@discocube.org>
2023-03-18 02:12:23 +0100

    removed last remaining while-loop for even less complexity.

--- src/graph/weave.rs
+++ src/graph/weave.rs
@@ -22,7 +22,7 @@ pub fn weave(
     z_order: &ZOrder,
     max_xyz: Point,
 ) -> Solution {
-    join_loops(
+    weave_loom(
         prepare_loom(vi_map, verts, z_adj, z_order),
         adj,
         verts,
@@ -32,7 +32,7 @@ pub fn weave(
 }
 
 fn prepare_loom(vi_map: &VIMap, verts: &Verts, z_adj: &Adjacency, z_order: &ZOrder) -> Loom {
-    let spool: Spool = spin_and_color_yarn(z_adj, verts);
+    let spool: Spool = spin_color_yarn(z_adj, verts);
     let mut bobbins: Bobbins = Vec::new();
     let mut loom: Loom = Loom::new();
     for (zlevel, order) in z_order {
@@ -40,24 +40,24 @@ fn prepare_loom(vi_map: &VIMap, verts: &Verts, z_adj: &Adjacency, z_order: &ZOrd
         let woven: Woven = join_threads(&mut loom, &warps);
         affix_loose_threads(&mut loom, warps, woven);
         if *zlevel != -1 {
-            bobbins = wind(&mut loom, verts, vi_map);
+            bobbins = wind_threads(&mut loom, verts, vi_map);
         }
     }
     reflect_loom(&mut loom, verts, vi_map);
     loom
 }
 
-fn spin_and_color_yarn(z_adj: &Adjacency, verts: &Verts) -> Spool {
-    let natural: Yarn = spin(z_adj, verts);
+fn spin_color_yarn(z_adj: &Adjacency, verts: &Verts) -> Spool {
+    let natural: Yarn = spin_yarn(z_adj, verts);
     let colored: Yarn = color_yarn(&natural);
     Spool::from([(3, natural), (1, colored)])
 }
 
-fn spin(z_adj: &Adjacency, verts: &Verts) -> Yarn {
+fn spin_yarn(z_adj: &Adjacency, verts: &Verts) -> Yarn {
     let path: &mut Tour = &mut vec![*z_adj.keys().max().unwrap() as Node];
     let order: Count = z_adj.len();
     (1..order).for_each(|idx| path.push(next_node(path, z_adj, verts, idx, order)));
-    convert_nodes_to_yarn(path, verts)
+    nodes_to_yarn(path, verts)
 }
 
 fn next_node(path: TourSlice, adj: &Adjacency, verts: &Verts, idx: usize, order: usize) -> Node {
@@ -84,7 +84,7 @@ fn next_node(path: TourSlice, adj: &Adjacency, verts: &Verts, idx: usize, order:
     }
 }
 
-fn convert_nodes_to_yarn(path: &mut Tour, verts: &Verts) -> Yarn {
+fn nodes_to_yarn(path: &mut Tour, verts: &Verts) -> Yarn {
     Yarn::from(
         path.iter()
             .map(|&n| [verts[n as usize].0, verts[n as usize].1])
@@ -96,7 +96,7 @@ fn color_yarn(a: &Yarn) -> Yarn {
     a.clone().dot(&ndarray::arr2(&[[-1, 0], [0, -1]])) + ndarray::arr2(&[[0, 2]])
 }
 
-fn wind(loom: &mut Loom, verts: &Verts, vi_map: &VIMap) -> Bobbins {
+fn wind_threads(loom: &mut Loom, verts: &Verts, vi_map: &VIMap) -> Bobbins {
     loom.iter_mut()
         .flat_map(|thread| {
             let (left, right) = get_upper_nodes(
@@ -122,7 +122,7 @@ fn get_warps(
     spool: &Spool,
     vi_map: &VIMap,
 ) -> Warps {
-    let node_yarn: Tour = preallocate_node_yarn(
+    let node_yarn: Tour = precut_node_yarn(
         spool[&(zlevel % 4 + 4).try_into().unwrap()].clone(),
         zlevel,
         order,
@@ -131,11 +131,11 @@ fn get_warps(
     if bobbins.is_empty() {
         vec![node_yarn]
     } else {
-        cut(node_yarn, bobbins)
+        cut_yarn(node_yarn, bobbins)
     }
 }
 
-fn preallocate_node_yarn(mut yarn: Yarn, zlevel: Point, order: Count, vi_map: &VIMap) -> Tour {
+fn precut_node_yarn(mut yarn: Yarn, zlevel: Point, order: Count, vi_map: &VIMap) -> Tour {
     yarn.slice_axis_inplace(
         ndarray::Axis(0),
         ndarray::Slice::new(
@@ -149,7 +149,7 @@ fn preallocate_node_yarn(mut yarn: Yarn, zlevel: Point, order: Count, vi_map: &V
         .collect()
 }
 
-fn cut(tour: Tour, subset: &Bobbins) -> Subtours {
+fn cut_yarn(tour: Tour, subset: &Bobbins) -> Subtours {
     let mut subtours: Subtours = Vec::new();
     let last_ix: Idx = tour.len() - 1;
     let last_idx: Idx = subset.len() - 1;
@@ -239,7 +239,7 @@ fn reflect_loom(loom: &mut Loom, verts: &Verts, vi_map: &VIMap) {
     });
 }
 
-pub fn join_loops<'a>(
+pub fn weave_loom<'a>(
     mut warp_wefts: Loom,
     adj: &'a Adjacency,
     verts: &'a Verts,
