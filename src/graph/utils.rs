a442855905e723db81a7229c10fb92075b41a2d9
Rommelo Yu <developer@discocube.org>
2023-03-22 01:49:29 +0100

    current refactoring is best so far... re: memory. keep it up!

--- src/graph/utils.rs
+++ src/graph/utils.rs
@@ -327,15 +327,10 @@ pub mod certify {
 }
 
 pub mod make_edges_eadjs {
-    use rayon::prelude::*;
     use super::{Edge, Edges, VIMap, Vert};
+    use rayon::prelude::*;
 
-    pub fn create_eadjs(
-        (a, b, c): Vert,
-        (x, y, z): Vert,
-        max_xyz: i16,
-        vi_map: &VIMap,
-    ) -> Edges {
+    pub fn create_eadjs((a, b, c): Vert, (x, y, z): Vert, max_xyz: i16, vi_map: &VIMap) -> Edges {
         match (a != x, b != y, c != z) {
             (true, false, false) => [[0, 2, 0], [0, -2, 0], [0, 0, 2], [0, 0, -2]],
             (false, true, false) => [[2, 0, 0], [-2, 0, 0], [0, 0, 2], [0, 0, -2]],
@@ -354,12 +349,7 @@ pub mod make_edges_eadjs {
         .collect()
     }
 
-    pub fn create_edges(
-        (a, b, c): Vert,
-        (x, y, z): Vert,
-        max_xyz: i16,
-        vi_map: &VIMap,
-    ) -> Edges {
+    pub fn create_edges((a, b, c): Vert, (x, y, z): Vert, max_xyz: i16, vi_map: &VIMap) -> Edges {
         match (a != x, b != y, c != z) {
             (true, false, false) => [[0, 2, 0], [0, -2, 0], [0, 0, 2], [0, 0, -2]],
             (false, true, false) => [[2, 0, 0], [-2, 0, 0], [0, 0, 2], [0, 0, -2]],
